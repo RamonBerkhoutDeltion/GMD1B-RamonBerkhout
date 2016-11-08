fba82c814c37a817f43dc23e29bb2f6081abe331
Ramon97 <ramonberkhout@live.nl>
2016-11-08 16:23:55 +0100

    project reupload

---
+++ P1/Flipperkast/Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float thrust;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            rb.velocity = (transform.forward * thrust);
+        }
+        
+    }
+}
---
+++ P1/Flipperkast/Assets/Scripts/SpawningBall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawningBall : MonoBehaviour {
+
+    public GameObject ball;
+    public Vector3 position;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        Instantiate(ball, position, Quaternion.identity);
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+}
